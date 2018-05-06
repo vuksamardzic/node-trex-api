673f1eaf019268d261b9347419a3f6099370365f
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-05-06 12:00:02 +0200

    dynamic PORT success

--- models/boards.model.js
+++ models/boards.model.js
@@ -1,16 +1,16 @@
-// const mongoose = require('mongoose');
-//
-// const schema = {
-//   name: {
-//     type: String,
-//     required: true
-//   }
-// };
-//
-// const boardSchema = mongoose.Schema(schema);
-//
-// const Board = module.exports = mongoose.model('board', boardSchema);
-//
-// module.exports.getBoards = function (cb) {
-//   Board.find(cb);
-// };
+const mongoose = require('mongoose');
+
+const schema = {
+  name: {
+    type: String,
+    required: true
+  }
+};
+
+const boardSchema = mongoose.Schema(schema);
+
+const Board = module.exports = mongoose.model('board', boardSchema);
+
+module.exports.getBoards = (next) => {
+  Board.find(next);
+};
