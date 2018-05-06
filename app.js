da57837a48de76575520acd02ddcf35ae520638c
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-05-06 11:28:06 +0200

    basic GET boards api

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
-var cors = require('cors');
-
 var app = express();
+var cors = require('cors');
+var mongoose = require('mongoose');
+var boards = require('./models/boards.model');
+mongoose.connect('mongodb://vs:12345@ds117070.mlab.com:17070/trex');
 
+var db = mongoose.connection;
 app.use(cors());
 
-app.get('/', function(req, res) {
-  res.json({ok: true});
+app.get('/api/v1/boards', function(req, res) {
+  boards.getBoards(function (err, data) {
+    if (err) {
+      throw err;
+    }
+    res.json(data);
+  });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(3000);
