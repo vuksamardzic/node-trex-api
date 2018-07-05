05070299a25555c77efb840cb48854c6a5154215
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-05 20:10:40 +0200

    Board controller and router.

--- src/api/resources/board/board.contoller.js
+++ src/api/resources/board/board.contoller.js
@@ -1,5 +1,54 @@
-import { generateControllers } from '../../modules/query';
-import { Board } from './board.model';
+import merge from 'lodash.merge';
 
-export default generateControllers(Board);
+const createOne = (model) => (req, res, next) => {
+  return model.create(req.body)
+    .then(docs => res.json(docs))
+    .catch(error => next(error));
+};
 
+const readAll = (model) => (req, res, next) => {
+  return model.find({}).exec()
+    .then(docs => res.json(docs))
+    .catch(error => next(error));
+};
+
+const readOne = () => (req, res) => {
+  return res.json(req.doc);
+};
+
+const findById = (model) => (req, res, next, id) => {
+  return model.findById(id).exec()
+    .then(doc => {
+      if (doc) {
+        req.doc = doc;
+        next();
+      } else {
+        next(new Error('Doc Not Found'));
+      }
+    })
+    .catch(error => next(error));
+};
+
+const updateOne = () => (req, res, next) => {
+  merge(req.doc, req.body);
+  req.doc.save()
+    .then(doc => res.json(doc))
+    .catch(error => next(error));
+};
+
+const deleteOne = () => (req, res, next) => {
+  req.doc.remove()
+    .then(doc => res.json({ message: `Board [${doc.name}] was deleted.` }))
+    .catch(error => next(error));
+};
+
+export const boardController = (model) => {
+  return {
+    createOne: createOne(model),
+    readOne: readOne(),
+    readAll: readAll(model),
+    findById: findById(model),
+    updateOne: updateOne(),
+    deleteOne: deleteOne()
+  };
+};
--- src/api/resources/board/board.router.js
+++ src/api/resources/board/board.router.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import boardController from './board.contoller';
+import { boardController } from './board.contoller';
+import { Board } from './board.model';
 
-export const boardRouter = express.Router();
+export const boardRouter = express.Router({});
 
-// boardRouter.param('id', boardController.findByParam);
+boardRouter.param('id', boardController(Board).findById);
 
 boardRouter.route('/')
-  .get(boardController.getAll)
-  .post(boardController.createOne);
+  .get(boardController(Board).readAll)
+  .post(boardController(Board).createOne);
 
 boardRouter.route('/:id')
-  .get(boardController.getOne)
-  .put(boardController.updateOne)
-  .delete(boardController.deleteOne);
+  .get(boardController(Board).readOne)
+  .put(boardController(Board).updateOne)
+  .delete(boardController(Board).deleteOne);
