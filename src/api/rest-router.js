c857af377824571bedea13ae6ae6a55033ba0d54
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-08 19:05:51 +0200

    card router and small cleanup.

--- src/api/rest-router.js
+++ src/api/rest-router.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { boardRouter } from './resources/board/board.router';
-import { apiErrorHandler } from './modules/error-handler';
 import { listRouter } from './resources/list/list.router';
+import { cardRouter } from './resources/card/card.router';
+import { apiErrorHandler } from './modules/error-handler';
+
 
 export const restRouter = express.Router({});
 
 restRouter.use('/board', boardRouter);
 restRouter.use('/list', listRouter);
+restRouter.use('/card', cardRouter);
 restRouter.use(apiErrorHandler);
--- src/server.js
+++ src/server.js
@@ -10,12 +10,7 @@ connect();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({ health: true });
-});
 app.use('/api/v1', restRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('live on port:', port);
-});
+app.listen(port);
